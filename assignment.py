926f5683c1e0454a5b4d1d4441acb58c66374115
minahiltariq265 <minahiltariq265@gmail.com>
2019-10-04 10:52:26 -0700

    My second commit

---
+++ assignment.py
@@ -0,0 +1,15 @@
+x=6
+print(x)
+y=x+7
+print(y)
+
+print('You must be the legendary god that comes from the sky')
+print('We have been waiting for you for a long time')
+print('Our legend says you will be very tasty with mustard')
+print('We will have a feast tonight unless you say')
+
+x=43
+x=x+1
+print(x)
+print('I hate you python')
+print('good bye')
