febc3719706c9c6182beb8dcb9b22c038daf8d3e
minahiltariq265 <minahiltariq265@gmail.com>
2019-10-11 07:15:13 -0700

    My third commit

---
+++ assignmet6.py
@@ -0,0 +1,74 @@
+print(4) 
+
+type('Hello, World!') 
+
+type(17) 
+
+type(3.2)
+
+type('17') 
+
+type('3.2') 
+
+print(1,000,000) 
+
+message = 'And now for something completely different' 
+n = 17  
+pi = 3.1415926535897931
+print(n) 
+print(pi) 
+type(message) 
+type(n)
+type(pi)
+
+print(1) 
+x = 2 
+print(x)
+
+minute = 59 
+minute/60 
+0.9833333333333333
+
+5 
+x = 5 
+x + 1
+
+remainder = 7 % 3
+print(remainder) 
+
+first = 10 
+second = 15 
+print(first+second)
+
+first = '100'
+second = '150'  
+print(first + second)
+
+first = 'Test ' 
+second = 3 
+print(first * second) 
+
+name = input('What is your name?\n')
+print(name) 
+
+prompt = 'What...is the airspeed velocity of an unladen swallow?\n' 
+speed = input(prompt)
+
+int(speed) 
+int(speed) + 5 
+
+a = 35.0 
+b = 12.50 
+c = a * b 
+print(c)
+
+hours = 35.0 
+rate = 12.50 
+pay = hours * rate 
+print(pay)
+
+x1q3z9ahd = 35.0 
+x1q3z9afd = 12.50 
+x1q3p9afd = x1q3z9ahd * x1q3z9afd 
+print(x1q3p9afd)
+
